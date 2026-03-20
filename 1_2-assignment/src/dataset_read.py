eac0b496bd49abb21a665df2adceeaca87520dfc
Hautzsi <tontsa.hautamaki@gmail.com>
2026-03-20 19:42:04 +0200

    dataset_analysis_update

--- 1_2-assignment/src/dataset_read.py
+++ 1_2-assignment/src/dataset_read.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-dataset1 = "./Datasets/fingrid/ElectricityProductionFinland_2026-02-17T0000_2026-03-17T0000.csv"
-dataset2 = "./Datasets/fingrid/ElectricityConsumptionFinland_2026-02-17T0000_2026-03-17T0000.csv"
-dataset3 = "./Datasets/fingrid/SmallScaleElectricitySurplus_2026-02-17T0000_2026-03-17T0000.csv"
+dataset1 = "../Datasets/fingrid/ElectricityProductionFinland_2026-02-17T0000_2026-03-17T0000.csv"
+dataset2 = "../Datasets/fingrid/ElectricityConsumptionFinland_2026-02-17T0000_2026-03-17T0000.csv"
+dataset3 = "../Datasets/fingrid/SmallScaleElectricitySurplus_2026-02-17T0000_2026-03-17T0000.csv"
 
-df = pd.read_csv(dataset1)
-df.head()
-df.info()
-df.describe()
+df = pd.read_csv(dataset3, sep=";")
+print("")
+print("Head: ")
+print(df.head())
+print("")
+print("Info: ")
+print(df.info())
+print("")
+print("Nulls: ")
+print(df.isnull().sum())
+print("")
+print("Describe: ")
+print(df.describe())
