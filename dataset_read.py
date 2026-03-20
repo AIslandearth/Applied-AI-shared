898c5e709f6c218e5a8fd3299b84f01e8671de11
Hautzsi <tontsa.hautamaki@gmail.com>
2026-03-20 18:57:16 +0200

    csv reader

---
+++ dataset_read.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+dataset1 = "./Datasets/fingrid/ElectricityProductionFinland_2026-02-17T0000_2026-03-17T0000.csv"
+dataset2 = "./Datasets/fingrid/ElectricityConsumptionFinland_2026-02-17T0000_2026-03-17T0000.csv"
+dataset3 = "./Datasets/fingrid/SmallScaleElectricitySurplus_2026-02-17T0000_2026-03-17T0000.csv"
+
+df = pd.read_csv(dataset1)
+df.head()
+df.info()
+df.describe()
+
